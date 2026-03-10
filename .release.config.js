81e050c646564da2e6fa31fe5eb55f815f9c15a6
Ahmed Elrayes <30704271+Ahmed-Elrayes@users.noreply.github.com>
2026-03-10 11:37:13 +0200

    feat: Implement automated releases with Semantic Release and remove an outdated example Blade template.

---
+++ .release.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    branches: ["main"],
+    tagFormat: "v${version}",
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        [
+            "@semantic-release/npm",
+            {
+                npmPublish: true
+            }
+        ],
+        [
+            "@semantic-release/git",
+            {
+                assets: ["package.json", "package-lock.json"],
+                message: "chore(release): ${nextRelease.version} [skip ci]"
+            }
+        ],
+        "@semantic-release/github"
+    ]
+};
